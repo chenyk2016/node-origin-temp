a1d14ebac0999f1462903661b19abba10820817e
陈亚昆(29389228) <chenyakun002@lianjia.com>
2022-10-02 15:09:17 +0800

    perf: 更新

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,14 @@
 import 'module-alias/register'
 
 // 注册环境变量
-import './common/env'
+import '@/common/env'
 
 // 全局错误捕获
-import './common/node-error-handle'
+import '@server/common/node-error-handle'
 import Task from '@server/task'
 
-import app from './router'
-import certificate from '../../public/certificate'
+import app from '@server/router'
+import certificate from '@/public/certificate'
 import https from 'https'
 
 const SERVER_CONFIG = process.env.SERVER_CONFIG
